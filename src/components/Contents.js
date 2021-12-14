41aab78e7757df36c24aca2c2016c89c90eea90a
Eunjee <58850132+wowww@users.noreply.github.com>
2021-12-14 13:01:58 +0900

    docs. Contents

---
+++ src/components/Contents.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contents = () => {
+  return (
+    <section>
+      <h2>국내 코로나 현황</h2>
+      <div className="contents">
+
+      </div>
+    </section>
+  )
+}
+
+export default Contents
